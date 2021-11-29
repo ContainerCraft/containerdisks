6738e3cd4c733a8772897c362a71db6dfbbdd447
jbpratt <jbpratt78@gmail.com>
2021-11-29 15:44:24 -0600

    commands-from-file and verbose curl

--- preview/ubuntu/env.sh
+++ preview/ubuntu/env.sh
@@ -1,2 +1,1 @@
 VIRT_SYSPREP_OPERATIONS=user-account,logfiles,customize,bash-history,net-hostname,net-hwaddr,machine-id,dhcp-server-state,dhcp-client-state,yum-uuid,udev-persistent-net,tmp-files,smolt-uuid,rpm-db,package-manager-cache
-VIRT_SYSPREP_INSTALL_PACKAGES=screenfetch,python3,tmux,git,vim,net-tools,cloud-init,cloud-initramfs-growroot,qemu-guest-agent
