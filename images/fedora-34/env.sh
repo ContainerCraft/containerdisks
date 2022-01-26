f5ed3bdaffc4c8caab6b8a1482d007e4c7bb6c53
jbpratt <jbpratt78@gmail.com>
2022-01-26 12:58:56 -0600

    ci: experiment with vm presets
    https://github.com/kubevirt/kubevirt/blob/main/docs/vm-presets.md
    
    Signed-off-by: jbpratt <jbpratt78@gmail.com>

--- images/fedora-34/env.sh
+++ images/fedora-34/env.sh
@@ -4,3 +4,4 @@ BASE_URL=https://download.fedoraproject.org/pub/fedora/linux/releases/34/Cloud/"
 DOWNLOAD_FILE=Fedora-Cloud-Base-34-1.2."${_ARCH}".qcow2
 AMD64_SHA256SUM=b9b621b26725ba95442d9a56cbaa054784e0779a9522ec6eafff07c6e6f717ea
 ARM64_SHA256SUM=141f16f52bfbe159947267658a0dbfbbe96fd5b988a95d1271f9c9ed61156da2
+SKIP="ssh"
