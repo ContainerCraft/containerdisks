a7185c69289f5d4f8056123497221ba7d875d773
jbpratt <jbpratt78@gmail.com>
2022-02-05 13:50:03 -0600

    fixup! ci: experiment with vm presets
    Signed-off-by: jbpratt <jbpratt78@gmail.com>

--- images/ubuntu-18.04/testvm.yaml.sh
+++ images/ubuntu-18.04/testvm.yaml.sh
@@ -1,17 +1,4 @@
 cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: ssh
-spec:
-  ports:
-  - nodePort: 30950
-    port: 30950
-    protocol: TCP
-    targetPort: 22
-  selector:
-    test: kmi
-  type: NodePort
 ---
 apiVersion: kubevirt.io/v1alpha3
 kind: VirtualMachine
@@ -25,15 +12,12 @@ spec:
     metadata:
       labels:
         test: kmi
+        kubevirt.io/flavor: default-features
     spec:
       nodeSelector:
         test: "kmi"
       domain:
         devices:
-          autoattachPodInterface: true
-          autoattachSerialConsole: true
-          autoattachGraphicsDevice: true
-          networkInterfaceMultiqueue: false
           disks:
             - name: containerdisk
               bootOrder: 1
@@ -42,11 +26,6 @@ spec:
             - name: cloudinitdisk
               disk:
                 bus: virtio
-        resources:
-          limits:
-            memory: 3G
-          requests:
-            memory: 3G
       hostname: testvm
       terminationGracePeriodSeconds: 0
       accessCredentials:
@@ -61,7 +40,7 @@ spec:
       volumes:
         - name: containerdisk
           containerDisk:
-            image: docker.io/containercraft/${FLAVOR/-/:}-dev
+            image: docker.io/containercraft/ubuntu:18.04-dev
             imagePullPolicy: Always
         - name: cloudinitdisk
           cloudInitNoCloud:
