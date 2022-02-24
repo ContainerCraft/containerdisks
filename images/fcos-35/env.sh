1f776eecba3184233ada09957fa045f9cb7f3fec
jbpratt <jbpratt78@gmail.com>
2022-02-24 09:33:57 -0600

    images: update fcos-35
    Signed-off-by: jbpratt <jbpratt78@gmail.com>

--- images/fcos-35/env.sh
+++ images/fcos-35/env.sh
@@ -1,8 +1,8 @@
 _ARCH=$(echo ${ARCH} | sed 's/amd64/x86_64/;s/arm64/aarch64/')
-_BUILD_VERSION="35.20220103.3.0"
+_BUILD_VERSION="35.20220131.3.0"
 BASE_URL=https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/"${_BUILD_VERSION}"/"${_ARCH}"
-DOWNLOAD_FILE=fedora-coreos-"${_BUILD_VERSION}"-openstack."${_ARCH}".qcow2.xz
-AMD64_SHA256SUM=3109807db69846049f7c4db85563fe596f2043493e3ded8cd7e9488afb94a9e5
-ARM64_SHA256SUM=bf6b4d3ed75d25f4d2573ceb867410ab46dcaa67d02f8f0074e8e5be5de0685a
+DOWNLOAD_FILE=fedora-coreos-"${_BUILD_VERSION}"-qemu."${_ARCH}".qcow2.xz
+AMD64_SHA256SUM=7342de6bbe9537e2e71415425706b1173704f5746aeeebfccff01435cccf0179
+ARM64_SHA256SUM=02de579dc9d9a5f1c1a1870aba16600f53a304b759743d74168ff2520313a9ce
 CUSTOMIZE=false
 SKIP="qemu-guest-agent|ssh"
