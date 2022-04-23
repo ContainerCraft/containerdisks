94048e0b11c8ed3967ec4c6d831b3d715c56fafb
usrbinkat <usrbinkat@braincraft.io>
2022-04-23 12:56:50 -0700

    add checksums
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- images/ubuntu-22.04/env.sh
+++ images/ubuntu-22.04/env.sh
@@ -2,5 +2,5 @@ VIRT_SYSPREP_OPERATIONS=user-account,logfiles,customize,bash-history,net-hostnam
 BASE_URL=https://cloud-images.ubuntu.com/jammy/current
 AMD64_DOWNLOAD_FILE=jammy-server-cloudimg-amd64-disk-kvm.img
 ARM64_DOWNLOAD_FILE=jammy-server-cloudimg-arm64.img
-AMD64_SHA256SUM=5d36ac526d12c60e34611fe1e1fbac5db9e0438c918344b885ded09874cbb30d
-ARM64_SHA256SUM=9b55f3a84eb03e1246d525505fda00abf338feea804dd756aab4426bc5066ccd
+AMD64_SHA256SUM=1593c433f220db3244326c0704e4217ec040e26b429adc87bc8039c35f61b5a4
+ARM64_SHA256SUM=731fd3d49500d9e5ba9ca6e9b23362e471cd34dcccfa81c619eb791fe09ee2fb
